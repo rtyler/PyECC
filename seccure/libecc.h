cd431be82b1d663d894e9f5b0b01322c8710f23d
R. Tyler Ballance <tyler@slide.com>
2009-06-10 14:02:07 -0700

    Flesh out the libecc interface via the header file and some doxygen doc tags
    Signed-off-by: R. Tyler Ballance <tyler@slide.com>

--- seccure/libecc.h
+++ seccure/libecc.h
@@ -20,6 +20,9 @@
 #ifndef _LIBECC_H_
 #define _LIBECC_H_
 
+#include <stdbool.h>
+
+
 /*
  * ecc_keygen(entropy)
  * ecc_encrypt(data, private)
@@ -27,6 +30,72 @@
  * ecc_sign(data, private)
  * ecc_verify(signeddata, public)
  */
+
+/**
+ * ::ECC_KeyPair denotes a structure to hold the public/private
+ * keys necessary for ECC sign/verify/encrypt and decrypting.
+ */
+struct {
+	void *priv;
+	void *pub;
+} _ECC_KeyPair;
+typedef struct _ECC_KeyPair* ECC_KeyPair;
+
+/**
+ * ::ECC_Data is simply a shortcut to an allocated void pointer
+ */
+struct {
+	void *data;
+} _ECC_Data;
+typedef struct _ECC_Data ECC_Data;
+
+
+/**
+ * Generate an ECC public/private key pair
+ *
+ * If the "priv" parameter isn't passed, then ecc_keygen() will generate
+ * it's own private key via some other source of entropy and then return
+ * the allocated key in the ::ECC_KeyPair
+ *
+ * @return ECC_KeyPair 
+ * @param priv Specify NULL to generate a random private key, otherwise string.
+ * You will be responsible for deallocating "priv" yourself, if not-NULL then
+ * the contents of the buffer will be copied into a new buffer
+ */
+ECC_KeyPair ecc_keygen(void *priv);
+
+
+/**
+ * Encrypt the specified block of data using the public key specified
+ *
+ * @return An allocated buffer with the encrypted data 
+ */
+ECC_Data ecc_encrypt(void *data, ECC_KeyPair keypair);
+
+
+/**
+ * Decrypt the specied block of data using the private key specified
+ *
+ * @return An allocated buffer with the decrypted data
+ */
+ECC_Data ecc_decrypt(void *encrypted, ECC_KeyPair keypair);
+
+
+/**
+ * Sign the specified block of data using the private key specified
+ *
+ * @return An allocated buffer with the signature of the data block
+ */
+ECC_Data ecc_sign(void *data, ECC_KeyPair keypair);
+
+
+/**
+ * Verify the signature of the data block using the specified public key
+ *
+ * @return True/False
+ */
+bool ecc_verify(void *data, void *signature, ECC_KeyPair keypair);
+
 
 
 #endif
