a39b7b2b9b0c9179d3aedcc29286cdcebf568d54
R. Tyler Ballance <tyler@slide.com>
2009-06-17 12:10:15 -0700

    Implement a bad sig test

--- tests.py
+++ tests.py
@@ -14,10 +14,16 @@ DEFAULT_PUBKEY = '8W;>i^H0qi|J&$coR5MFpR*Vn'
 DEFAULT_PRIVKEY = 'my private key'
 
 class ECC_Verify_Tests(unittest.TestCase):
+    def setUp(self):
+        super(ECC_Verify_Tests, self).setUp()
+        self.ecc = pyecc.ECC(public=DEFAULT_PUBKEY, private=DEFAULT_PRIVKEY)
+
     def test_BasicVerification(self):
-        ecc = pyecc.ECC(public=DEFAULT_PUBKEY, private=DEFAULT_PRIVKEY)
-        
-        assert ecc.verify(DEFAULT_DATA, DEFAULT_SIG), ('Failed to verify signature',
+        assert self.ecc.verify(DEFAULT_DATA, DEFAULT_SIG), ('Failed to verify signature',
+                DEFAULT_DATA, DEFAULT_SIG, DEFAULT_PUBKEY, DEFAULT_PRIVKEY)
+
+    def test_BadVerification(self):
+        assert self.ecc.verify(DEFAULT_DATA, "FAIL") == False , ('Verified on a bad sig',
                 DEFAULT_DATA, DEFAULT_SIG, DEFAULT_PUBKEY, DEFAULT_PRIVKEY)
 
 
