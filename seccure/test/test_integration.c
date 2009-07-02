310798b48d0092ee5738c793a10399dfdf21db75
R. Tyler Ballance <tyler@slide.com>
2009-07-02 10:33:27 -0700

    Working on (currently failing) test to verify the ecc_encrypt() results' consistency

--- seccure/test/test_integration.c
+++ seccure/test/test_integration.c
@@ -28,6 +28,8 @@
 #include "libseccure.h"
 
 
+#define LOOPS 100
+
 void __test_keygen_to_sign()
 {
 }
@@ -36,6 +38,93 @@ void __test_keygen_to_encrypt()
 {
 }
 
+void print_data(ECC_Data d, bool aschar)
+{
+	unsigned int i = 0;
+	if (!d->datalen)
+		return;
+
+	for (; i < d->datalen; ++i) {
+		if (aschar) 
+			fprintf(stderr, "%c ", ((char *)d->data)[i]);
+		else
+			fprintf(stderr, "%d ", (int)((char *)d->data)[i]);
+	}
+}
+void assert_eqaul_data(ECC_Data left, ECC_Data right)
+{
+	unsigned int i = 0;
+	char lc, rc;
+	if ( (!left) || (!right) ) {
+		fprintf(stderr, "left/right pointers (%p, %p)\n", left, right);
+		goto abort;
+	}
+
+	if ( left->datalen != right->datalen ) {
+		fprintf(stderr, "Non-equal `datalen` members\n");
+		goto abort;
+	}
+
+	for (; i < left->datalen; ++i) {
+		lc = ((char *)left->data)[i];
+		rc = ((char *)right->data)[i];
+
+		if ( lc != rc ) {
+			fprintf(stderr, "Character mismatch at char %d, left/right: %d/%d\n", 
+					i, (int)lc, (int)rc);
+			goto abort;
+		}	
+	}
+
+	return;
+
+	abort:
+		if (left && right) {
+			fprintf(stderr, "\t\tLeft\t||\tRight\n");
+			fprintf(stderr, "pointer\t %p \t||\t %p\n", left, right);
+			fprintf(stderr, "len\t %d \t||\t %d\n", left->datalen, right->datalen);
+			fprintf(stderr, "str\t ");
+			print_data(left, false);
+			fprintf(stderr, " \t||\t ");
+			print_data(right, false);
+			fprintf(stderr, "\n");
+		}
+		abort();
+}
+
+void __test_keygen_encryptsalot()
+{
+	ECC_State state = ecc_new_state(NULL);
+	ECC_KeyPair keypair = ecc_keygen(NULL, state);
+	ECC_Data encrypted, last_run;
+	unsigned int i = 0;
+	const char *data = "This should be all encrypted and such\n";
+	encrypted = last_run = NULL;
+
+	g_assert(keypair != NULL);
+	g_assert(keypair->pub != NULL);
+	g_assert(keypair->priv != NULL);
+
+	for (; i < LOOPS; ++i) {
+		fprintf(stderr, "%d, ", i);
+		encrypted = ecc_encrypt(data, strlen(data), keypair, state);
+
+		g_assert(data != NULL);
+		g_assert(encrypted != NULL);
+		g_assert(encrypted->data != NULL);
+
+		if (last_run == NULL) {
+			last_run = encrypted;
+		}
+		else {
+			assert_eqaul_data(encrypted, last_run);
+			ecc_free_data(last_run);
+			last_run = encrypted;
+		}
+	}
+
+	ecc_free_data(last_run);
+}
 
 int main(int argc, char **argv)
 {
@@ -43,6 +132,7 @@ int main(int argc, char **argv)
 
 	g_test_add_func("/integration/keygen_to_sign", __test_keygen_to_sign);
 	g_test_add_func("/integration/keygen_to_encrypt", __test_keygen_to_encrypt);
+	g_test_add_func("/integration/keygen_encryptsalot", __test_keygen_encryptsalot);
 
 	return g_test_run();
 }
