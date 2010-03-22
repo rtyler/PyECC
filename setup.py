44ea3f0b039cadd11f17f672f8ca593b82ac6472
R. Tyler Ballance <tyler@monkeypox.org>
2010-03-22 05:03:40 -0700

    Updating setup.py to build properly on FreeBSD

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+
 import os
-from distutils.core import setup, Extension
+
+SETUPTOOLS = False
+
+try:
+    from setuptools import setup, Extension
+    SETUPTOOLS = False
+except ImportError:
+    from distutils.core import setup, Extension
 
 base_modules = [
     Extension('_pyecc', [
@@ -14,7 +23,9 @@ base_modules = [
             'py_objects.c',
         ],
         libraries=['gcrypt'],
-        extra_compile_args=['-Wall', '-Werror'])
+        include_dirs=['/usr/include', '/usr/local/include',],
+        library_dirs=['/usr/local/lib', '/usr/local/lib64',],
+        extra_compile_args=['-Wall', '-Werror',]),
 ]
 
 packages = ['pyecc']
@@ -25,13 +36,13 @@ missing_modules = filter(lambda m: m not in modules, base_modules)
 if missing_modules:
 	print 'WARNING: Some Python modules are missing dependencies: %s' % ', '.join(map(lambda x: x.name, missing_modules))
 
-setup(
+kwargs = dict(
     name = 'PyECC',
     description = '''A CPython module to enable Elliptical Curve Cryptography in Python''',
     version = '1.0',
     author = 'R. Tyler Ballance',
-    author_email = 'tyler@slide.com',
-    ext_modules=modules, 
+    author_email = 'tyler@monkeypox.org',
+    ext_modules=modules,
     py_modules=['pyecc'])
 
-
+setup(**kwargs)
